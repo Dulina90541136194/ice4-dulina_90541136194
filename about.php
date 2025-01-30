5d134bb8e8ff9ba6855e8a9da660501e51c501d2
Dulina90541136194 <156538769+Dulina90541136194@users.noreply.github.com>
2025-01-29 20:07:51 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+ 
+<main>
+    <h1>About Us</h1><br><br>
+    <h2>Our Team</h2><br>
+    <ul>
+        <li>
+            <img src="images\my_image.jpg" alt="Dulina Sanketh" width="200" height="200">
+            <h3>Dulina Sanketh - Designer</h3><br>
+            <p class= "para2">Dulina is the creative mind behind the website's look and feel. He focuses on crafting visually appealing designs, intuitive layouts, and an engaging user interface. By combining aesthetics with usability, he ensures that visitors have a smooth and enjoyable browsing experience. His design choices enhance the website's identity and make it both functional and attractive.</p>
+        </li>
+        <li>
+            <img src="images\joshua.jpg"alt="Joshua Castillo" width="200" height="200">
+            <h3>Joshua Castillo - Developer</h3><br>
+            <p class= "para2">Joshua is the backbone of the website's functionality, handling the coding and technical aspects of the project. With expertise in PHP, HTML, CSS, and JavaScript, he ensures that the website runs smoothly, loads efficiently, and delivers a seamless user experience. From backend logic to front-end interactions, Joshua brings the site to life with his development skills.</p>
+        </li>
+        <li>
+            <img src="images\zhou.jpg"alt="Zhou Yu" width="200" height="200">
+            <h3>Zhou Yu - Product Manager</h3><br>
+            <p class= "para2">Zhou Yu plays a crucial role in overseeing the project's direction and execution. As the product manager, he coordinates between the development and design teams, ensuring that the website meets its goals and aligns with user needs. He manages timelines, sets priorities, and ensures that the final product is polished, user-friendly, and effective. His leadership keeps the project on track from start to finish.</p>
+        </li>
+    </ul>
+</main>
+<?php include 'footer.php'; ?>
---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Php Website By Dulina.  © All rights Reserved</p>
+</footer>
